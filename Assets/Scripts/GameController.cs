2c7b49d493435bb4995302a7a84dab0610c7836f
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-01-22 09:49:06 +0100

    Did basic gamecontroller ticking and instanceing

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController: MonoBehaviour
+{
+
+	public static GameController curr {
+		get;
+		private set;
+	}
+	public event System.Action Tick;
+	public int ticksPerMinute = 70;
+	private float TickTime
+	{
+		get
+		{
+			return 60f/ticksPerMinute;
+		}
+	}
+	private float nextTick;
+
+	void Awake() // Awake() går innan Start()
+	{
+		if ( curr != null )
+			throw new System.Exception( "Too many instances of GameController, should only be one" );
+		curr = this;
+	}
+
+	private void DoTick()
+	{
+		Tick.Invoke();
+		nextTick += TickTime;
+	}
+
+	void Update()
+	{
+		if ( Time.time > nextTick )
+			DoTick();
+	}
+}
