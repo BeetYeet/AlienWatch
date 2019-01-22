42a054cd92dde042f853b6e95855ec8b1f40bb6a
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-01-22 09:39:55 +0100

    mana bug hp wip

---
+++ Assets/scripts/PlayerHeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHeath : MonoBehaviour
+{
+    public int PlayerHPMax = 100;
+    public int PlayerHP = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
