34958243eac944c3fd5d0ff2992d515a58672d0d
Noel Mortensen <noel.mortensen@elev.ga.lbs.se>
2019-02-05 10:44:18 +0100

    Gjorde ett checkpoint script och prefaba checkpointen. gjorde ett GameMaster script och player position script. La till en tag "GM"

---
+++ Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
