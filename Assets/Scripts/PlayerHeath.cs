f425891d25a79645c81028352668d022f9d13754
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-02-05 08:37:08 +0100

    Small changes to damage and health

--- Assets/Scripts/PlayerHeath.cs
+++ Assets/Scripts/PlayerHeath.cs
@@ -2,40 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHeath : Damageble
+public class PlayerHeath: Damageble
 {
-    public int PlayerHPMax = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
+	public int healthMax = 100;
+	public bool dead
+	{
+		get
+		{
+			return health == 0;
+		}
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (health == 0)
-        {
-            //die
-        }
-        if (health < PlayerHPMax)
-        {
-            // can heal
-        }
-    }
-    public override void DoDamage(DamageInfo info)
-    {
-
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if ( health == 0 )
+		{
+			//die
+		}
+		if ( health < PlayerHPMax )
+		{
+			// can heal
+		}
+	}
+	public override void DoDamage( DamageInfo info )
+	{
+		if ( info.faction == Faction.Player )
+		{
+			health -= info.damage;
+			if ( health <= 0 )
+			{
+				health = 0;
+			}
+		}
+	}
 }
 
 public struct DamageInfo
 {
-    Faction faction;
-    int damage;
+	public Faction faction;
+	public int damage;
 }
 public enum Faction
 {
-    Player,
-    Alien
+	Player,
+	Alien
 }
