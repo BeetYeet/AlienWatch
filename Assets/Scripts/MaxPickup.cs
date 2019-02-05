55ff00b504e44fb5d30c23d3cb5bd39e47f4c42c
Noel Mortensen <noel.mortensen@elev.ga.lbs.se>
2019-02-05 09:04:13 +0100

    Döpte om skriptnamn i MaxPickup.

---
+++ Assets/Scripts/MaxPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxPickup : MonoBehaviour
+{
+    PlayerHeath playerHealth;
+
+    PlayerMana playerMana;
+
+    private void Awake()
+    {
+        playerHealth = FindObjectOfType<PlayerHeath>();
+        playerMana = FindObjectOfType<PlayerMana>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playerHealth.PlayerHP < playerHealth.PlayerHPMax)
+        {
+            Destroy(gameObject);
+            playerHealth.PlayerHP = playerHealth.PlayerHPMax;
+            playerMana.mana = playerMana.maxMana;
+        }
+    }
+}
