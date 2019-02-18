f6e99596f1c417c5e5c205620b8c2278e44452c3
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-02-18 10:32:11 +0100

    fuck u alfred

---
+++ Assets/Scripts/SwordObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Sword", fileName = "new Sword")]
+[System.Serializable]
+public class SwordObject : ScriptableObject
+{
+    public Sprite SwordArt;
+    [Header("Floats")]
+    [Space]
+    public float MeleeTime;
+    public float RotationLeft;
+    public float RotationRight;
+    [Header("Ints")]
+    [Space]
+    public int Damage;
+    [Header("Vectors")]
+    [Space]
+    public Vector3 defaultPos;
+    public Vector3 forwardPos;
+    public Vector3 swipeLeftPos;
+    public Vector3 SwipeRightPos;
+    [Header("Sword Return")]
+    [Space]
+    public SwordReturn Return;
+}
+public enum SwordReturn
+{
+    DontReturn,
+    ReturnLeft,
+    ReturnRight
+}
