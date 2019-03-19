8c2a1661bd775302eed8448f3f6d71a28b7df5df
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-03-19 09:55:31 +0100

    Menu Stuff

--- Assets/Scripts/UI/MenuSkript.cs
+++ Assets/Scripts/UI/MenuSkript.cs
@@ -5,16 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuSkript : MonoBehaviour
 {
-  
-    
+
+
     public GameObject main;
-    
+    public GameObject Pannel;
     public GameObject credd;
+    public float targetTimeScale;
+    public float NotMainTimeScale;
+    public float changeFactor = .2f;
 
     private void Start()
     {
-        main.SetActive(true);
+       // main.SetActive(true);
         credd.SetActive(false);
+    }
+
+    private void Update()
+    {
+       Time.timeScale = FloatLerp(Time.timeScale, targetTimeScale, changeFactor);
+        if (credd.activeInHierarchy == false)
+            targetTimeScale = 1;
     }
 
     public void Quit()
@@ -29,13 +39,24 @@ public class MenuSkript : MonoBehaviour
 
     public void Credits()
     {
+        // ACTIVATES MAINMENU
         main.SetActive(false);
+        // ACTIVATES CREDDITS
         credd.SetActive(true);
+        targetTimeScale = NotMainTimeScale;
+
+    }
+
+    float FloatLerp(float a, float b, float v)
+    {
+        float diff = b - a;
+        return a + diff * v;
     }
 
     public void BackCred()
     {
         main.SetActive(true);
+        targetTimeScale = 1f;
         credd.SetActive(false);
     }
 }
