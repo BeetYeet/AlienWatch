d26c0ee99ffa9ad36ea7efbd2aec512ccfaf065a
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-04-22 18:20:45 +0200

    smol changes

--- Assets/Scripts/DebugScript.cs
+++ Assets/Scripts/DebugScript.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DebugScript : MonoBehaviour
+public class DebugScript: MonoBehaviour
 {
 
-    void Update()
-    {
-		if (Input.GetKeyDown(KeyCode.R))
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+	void Update()
+	{
+		if ( Input.GetKeyDown( KeyCode.R ) )
+		{
+			Time.timeScale = 1f;
+			SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+		}
+	}
 }
