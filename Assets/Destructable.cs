d9242e8829145b96887b3538c60fbbac9025bb71
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-05-07 09:03:59 +0200

    hi

--- Assets/Destructable.cs
+++ Assets/Destructable.cs
@@ -14,6 +14,7 @@ public class Destructable : Damageble
 	{
 		CurrentSprite = GetComponent<SpriteRenderer>();
 		//GameController.ClampToGrid(transform.position);
+		CurrentSprite.sprite = Una;
 	}
 
 	public override void DoDamage(DamageInfo info)
