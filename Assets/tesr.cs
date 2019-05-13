4236357b752e4e356cddb6ae4e50dc5ea6247efc
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-05-13 10:05:58 +0200

    added stuff

---
+++ Assets/tesr.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tesr : MonoBehaviour
+{
+	Renderer material;
+	public float test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		material = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
