984209bdd573743623abc2fc1646f68cb7919e73
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-05-13 11:05:36 +0200

    fixed shit

--- Assets/Scripts/AnimationHelper.cs
+++ Assets/Scripts/AnimationHelper.cs
@@ -3,13 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationHelper : MonoBehaviour
+public class AnimationHelper: MonoBehaviour
 {
 	public Animator anim;
 	float lastX = 0f;
 	float lastY = -.25f;
 	void Update()
 	{
+		if ( Time.timeScale == 0f )
+		{
+			anim.SetFloat( "X", 0f );
+			anim.SetFloat( "Y", -.25f );
+			return;
+		}
 		float X = Input.GetAxis( "Horizontal" );
 		float Y = Input.GetAxis( "Vertical" );
 		if ( Mathf.Abs( X ) > .20f || Mathf.Abs( Y ) > .20f )
